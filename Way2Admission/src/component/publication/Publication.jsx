a43d0600b95cb569dd6dd2e41e1917d7911c6003
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-21 16:47:49 +0530

    carousel complete

---
+++ Way2Admission/src/component/publication/Publication.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Publication() {
+    return (
+        <div className='publication flex-center'>
+            Paper Will be Published Scopus AIP Conference Proceeding Publication
+        </div>
+    )
+}
