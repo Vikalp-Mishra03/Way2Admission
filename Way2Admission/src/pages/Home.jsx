33de7782e80fe078546e5ed073e12fcd66fe3e32
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-21 14:01:00 +0530

    starting new project

---
+++ Way2Admission/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contact from '../component/contact/Contact'
+import Navbar from '../component/navbar/Navbar'
+
+export default function Home() {
+    return (
+        <div>
+            <Contact />
+            <Navbar />
+        </div>
+    )
+}
