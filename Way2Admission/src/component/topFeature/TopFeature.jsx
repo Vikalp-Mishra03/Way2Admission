867a9e0ad534f503408e032759ede827acc5525c
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-22 15:03:07 +0530

    top feature section complete

--- Way2Admission/src/component/topFeature/TopFeature.jsx
+++ Way2Admission/src/component/topFeature/TopFeature.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import card1 from '../../assets/images/—Pngtree—career woman playing laptop on_5315939 1 (1).png'
 import card2 from '../../assets/images/—Pngtree—career woman playing laptop on_5315939 1 (2).png'
 import card3 from '../../assets/images/—Pngtree—career woman playing laptop on_5315939 1.png'
+
+
+const ReadMore = ({ children }) => {
+    const text = children;
+    const [isReadMore, setIsReadMore] = useState(true);
+    const toggleReadMore = () => {
+        setIsReadMore(!isReadMore);
+    };
+    return (
+        <p className="para">
+            {isReadMore ? text.slice(0, 100) : text}
+            <span
+                onClick={toggleReadMore}
+                className="read-or-hide"
+                style={{ color: "green" }}
+            >
+                {isReadMore ? "...read more" : " show less"}
+            </span>
+        </p>
+    );
+};
+
+
 export default function TopFeature() {
 
     const cards = [
@@ -23,8 +46,8 @@ export default function TopFeature() {
     ]
     return (
         <div className='topFeature '>
-            <h3>Our Top Class Features</h3>
-            <p>Building Strong Foundations That Empowers Young Mind Unraveling Learning In Our Educational Environment
+            <h3 className='heading'>Our Top Class Features</h3>
+            <p className=''>Building Strong Foundations That Empowers Young Mind Unraveling Learning In Our Educational Environment
             </p>
 
             <div className="box">
@@ -34,10 +57,10 @@ export default function TopFeature() {
                             <img src={card.img} alt={`Card ${index + 1}`} />
                         </div>
                         <div className="title">{card.title}</div>
-                        <div className="para">{card.para}</div>
+                        <ReadMore className="para">{card.para}</ReadMore>
                         <div className='link'>
                             <a href="#" className='learn'>Learn More</a>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#1f4282" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#1f4282" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" /></svg>
                         </div>
                     </div>
                 ))}
