c58500f59e3326fd01d0baf7f9488180d0343b4b
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-27 10:10:31 +0530

    home1 is complete

---
+++ src/pages/Home1.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Contact1 from '../component/home page 1/contact1/Contact1'
+import UpperBanner from '../component/home page 1/upper banner/UpperBanner'
+import Updates from '../component/home page 1/updates/Updates'
+import About1 from '../component/home page 1/about1/About1'
+import Card3 from '../component/home page 1/card3/Card3'
+import Admission from '../component/home page 1/admission/Admission'
+import TopFeature from '../component/home pge/topFeature/TopFeature'
+import Stories from '../component/home pge/stories/Stories'
+import WhyChooseWay2 from '../component/home pge/whyChooseway2/WhyChooseWay2'
+import Video from '../component/home pge/video/Video'
+import EventAndNews from '../component/home pge/eventandNews/EventAndNews'
+import Partners from '../component/home pge/partners/Partners'
+import Accordion from '../component/home pge/accordion/Accordion'
+
+export default function Home1() {
+    return (
+        <div>
+            <Contact1 />
+            <UpperBanner />
+            <Updates />
+            <About1 />
+            <Card3 />
+            <Admission />
+            <TopFeature />
+            <Stories />
+            <WhyChooseWay2 />
+            <Video />
+            <EventAndNews />
+            <Partners />
+            <Accordion />
+        </div>
+    )
+}
