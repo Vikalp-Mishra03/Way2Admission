f48eac266e831d5b30f1020bc7a9f52effae43e6
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-27 15:16:58 +0530

    about merge

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import AboutBanner from '../component/aboutUs/aboutbanner/AboutBanner';
+import OurStories from '../component/aboutUs/ourStories/OurStories';
+import Progress from '../component/aboutUs/progress/Progress';
+import OurOffering from '../component/aboutUs/ourOffering/OurOffering';
+import KnowMore from '../component/aboutUs/knowMore/KnowMore';
+import OurStudents from '../component/aboutUs/ourStudents/OurStudents';
+import Companies from '../component/aboutUs/companies/Companies';
+
 
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <AboutBanner />
+      <OurStories />
+      <Progress />
+      <OurOffering />
+      <Companies />
+      <KnowMore />
+      <OurStudents />
     </div>
   )
 }
