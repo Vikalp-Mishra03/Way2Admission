ad0abb450d347aca2a6162329d5bd04f125cc6fe
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-28 10:07:20 +0530

    our expert page complete

---
+++ src/pages/OurExpert.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ExpertUpper from '../component/expert/ExpertUpper'
+import Count from '../component/expert/Count'
+import MeetExpert from '../component/expert/MeetExpert'
+import SmallBanner from '../component/expert/SmallBanner'
+import ForApp from '../component/expert/ForApp'
+
+
+export default function OurExpert() {
+  return (
+    <div>
+      <ExpertUpper />
+      <Count />
+      <MeetExpert />
+      <ForApp />
+      <SmallBanner />
+    </div>
+  )
+}
