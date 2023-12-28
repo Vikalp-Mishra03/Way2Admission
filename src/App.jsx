fe5ab5c401bcec06387844aefbc47fc2cdae77eb
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-28 19:00:37 +0530

    adding new link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Contact from './component/contact/Contact'
 import Navbar from './component/navbar/Navbar'
 import Footer from './component/Footer/Footer'
+import About from './pages/About'
+import Services from './pages/Services'
+import Home1 from './pages/Home1'
+import OurExpert from './pages/OurExpert'
+import Termsandcondition from './pages/Termsandcondition'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import Career from './pages/Career'
+import ContactUs from './pages/ContactUs'
+import ResponsibleDisclosurePolicy from './pages/ResponsibleDisclosurePolicy'
 
 export default function App() {
   return (
@@ -12,6 +21,15 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/home1' element={<Home1 />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/expert' element={<OurExpert />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/termsAndCondition' element={<Termsandcondition />} />
+        <Route path='/privacyPolicy' element={<PrivacyPolicy />} />
+        <Route path='/career' element={<Career />} />
+        <Route path='/contact' element={<ContactUs />} />
+        <Route path='/responsible' element={<ResponsibleDisclosurePolicy/>} />
       </Routes>
       <Footer />
     </Router>
