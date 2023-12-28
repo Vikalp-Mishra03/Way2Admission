9424971e2968b7ee730c5a94eeb029c78330e9fa
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-28 18:58:46 +0530

    adding privacy policy page

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+import policy from '../assets/images/privacyPolicy.png'
+
+export default function PrivacyPolicy() {
+    return (
+        <div className="policy">
+            <div className='terms'>
+                <div className="back">
+                    <img src={policy} alt="" />
+                </div>
+
+            </div>
+            <div className="container">
+                <div className="termHeading">Privacy Policy</div>
+
+                <div className="table">
+                    <div className="main">Table of contents :</div>
+
+                    <ol >
+                        <li className='ml-1'><a href="#content1">Why we collect Personal information</a></li>
+                        <li className='ml-1'><a href="#content2"> What information do we collect and how do we use it</a></li>
+                        <li className='ml-1'><a href="#content3">Information Sharing</a></li>
+                        <li className='ml-1'><a href="#content4"> How long do we retain user data
+                        </a></li>
+                        <li className='ml-1'><a href="#content5">Grievances</a></li>
+                        <li className='ml-1'><a href="#content6">Disclaimer</a></li>
+                        <li className='ml-1'><a href="#content7">Data Deletion</a></li>
+                        <li className='ml-1'><a href="#content8">Grievance Redressal</a></li>
+                    </ol>
+
+                </div>
+
+                <div className="para1">
+                    <div className="para-heading" id='content1'>Why we collect Personal information?</div>
+                    <div className="para">
+                        We collect personal information from you to identify you, to provide you with information required to service you, and to help us deliver a superior career enhancement experience. Information collected enables us to give you convenient access to our service offerings and products, and may help us tailor your career experience.
+                    </div>
+
+                </div>
+                <div className="para1">
+                    <div className="para-heading" id='content2'>
+                        What information do we collect and how do we use it?
+                    </div>
+                    <div className="para">
+                        We collect information to provide more effective services. We ask for your name, email address, and other personal information when you register to use certain features of our website. We also may ask for this information at other times, such as when you enter contests or other promotions sponsored by us and/or our partners. If you use a feature that requires payment of a fee, we collect your credit card data or payment account information.
+                        <br />
+                        <br />
+                        When you use one of our paid products, we track the web pages, and information that has been accessed by you, and store it on our servers. This enables us to track items that you have completed, and those that you need to see.
+                    </div>
+                </div>
+
+                <div className="para1">
+                    <div className="para-heading" id='content3'>Information Sharing</div>
+                    <div className="para">
+                        Other than the above mentioned sharing of data related to credit card information, we only share personal information with other companies or individuals outside of Leverage EdTech Private Limited in the following limited circumstances:
+                    </div>
+                    <br />
+                    <li className="para ">
+                        Satisfy any applicable law, regulation, legal process or enforceable governmental request
+                    </li>
+                    <br />
+                    <li className="para ">
+                        Protect against imminent harm to the rights, property or safety of leverageedu.com EdTech Private Limited, its users or the public as required or permitted by law. We may share with third parties certain pieces of aggregated, non-personal information, such as the number of users subscribed to a particular product, or the number of users registered on our website. Such information does not identify you individually.
+                    </li>
+                    <br />
+                    <li className="para ">
+                        By using the website and/or registering yourself with us you authorise us to contact you via email or phone call or sms and offer you our services, imparting product knowledge, offer promotional offers running on website & offers offered by third parties, for which reasons, personally identifiable information may be collected. And irrespective of the fact if you have also registered yourself under DND or DNC or NCPR service, you still authorise us to give you a call from LeverageEdu for the above mentioned purposes till 365 days of your registration with us.
+
+                    </li>
+                    <br />
+                    <li className="para ">
+                        To address customer goals, requirements, service/product offerings as and when opted by the customer.
+                    </li>
+
+
+                </div>
+
+
+                <div className="para1">
+                    <div className="para-heading" id='content4'>How long do we retain user data?</div>
+                    <div className="para">
+                        We may provide information to service providers to help us bring you the services we offer. Specifically, we may use third parties to facilitate our business, such as to host the service at a co-location facility for servers. Where we utilize third parties for the processing of any personal information, we implement reasonable contractual and technical protections limiting the use of that information to our specified purposes.
+                    </div>
+
+                </div>
+
+                <div className="para1">
+                    <div className="para-heading" id='content5'>Grievances</div>
+                    <div className="para">
+                        If You have any grievances as to the usage of any of Your personal information or any other privacy concerns, You can contact the Grievance Officer, Aditya Arora whose details are provided below:
+                        <br />
+                        <a href="#" className='primary'>grievance@cnvk.com</a>
+                    </div>
+                </div>
+
+
+                <div className="para1">
+                    <div className="para-heading" id='content6'>Disclaimer</div>
+                    <div className="para">
+                        The information contained in this website is for general information purposes only. The information is provided by Leverage EdTech Private Limited and while we endeavor to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitabsility or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+                    </div>
+                    <br />
+                    <div className="para">
+                        In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arise out of, or in connection with, the use of this website.
+                    </div>
+                    <br />
+                    <div className="para">
+                        Through this website/ you are able to link to other websites which are not under the control of Leverage EdTech Private Limited. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+                    </div>
+                    <div className="para">
+                        Every effort is made to keep the website up and running smoothly. However, Leverage EdTech Private Limited takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.
+                    </div>
+                </div>
+
+                <div className="para1">
+                    <div className="para-heading" id='content7'>
+                        Data Deletion
+                    </div>
+                    <div className="para">
+                        You can send your data deletion request to us through email. In order to get your data deleted from our servers, send an email to datadel@leverageedu.com with a subject "Data Deletion Your Email ID".
+                    </div>
+
+                </div>
+
+                <div className="para1">
+                    <div className="para-heading" id='content8'>
+                        Grievance Redressal
+                    </div>
+                    <div className="para" >
+                        In compliance with the provisions of the Information and Technology Act, 2005 (“IT Act”), we have appointed a Grievance Officer, to address your concerns pertaining to the usage of your user data, you can contact the officer, Aditya Arora at grievance@leverageedu.com.
+                    </div>
+
+                    <address>In the event, you have any complaints or concerns regarding the usage of your user data, you may write to us electronically at grievance@leverageedu.com, or send a letter to us at: <br />
+                        Leverage Edu 3rd floor, <br />
+                        Plot number, 1-C, Raipur Khadar, <br />
+                        Sector 126, Noida, Uttar Pradesh <br />
+                        201313</address>
+                </div>
+            </div>
+        </div>
+    )
+}
