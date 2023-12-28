6fe447d49c86522ad09b8a892eadea96aa4a2f67
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-28 18:58:22 +0530

    adding career page

---
+++ src/pages/Career.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CareerMain from '../component/career/CareerMain'
+import JoinOurTeam from '../component/career/JoinOurTeam'
+import Life from '../component/career/Life'
+import WhyWork from '../component/career/WhyWork'
+
+export default function Career() {
+    return (
+        <div>
+            <CareerMain />
+            <JoinOurTeam />
+            <Life />
+            <WhyWork />
+        </div>
+    )
+}
